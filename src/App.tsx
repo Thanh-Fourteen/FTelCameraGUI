6ad386aa2cf191a1dbcb61b805614eac18c75bf8
Thanh-Fourteen <thanhfourteen@gmail.com>
2025-12-03 15:36:27 +0000

    v1

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { useRoutes } from 'react-router-dom'
-import {routesConfig} from './routes'
-
-function App() {
-  const element = useRoutes(routesConfig)
-  return <>{element}</>
-}
-export default App
--- src/types/collection.ts
+++
@@ -1,6 +0,0 @@
-export interface CreateCollectionPayload {
-    name: string;
-    vector_size: number;
-    distance: string;
-    id_type: string;
-}
